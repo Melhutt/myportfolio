7d8e14a9d0e1c45495889ebf313e5499f249cc53
mel <melhutt@virginmedia.com>
2015-08-04 17:14:41 +0100

    Add js/.

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+
+// alert("this is test");
+
+//Attach an event listener to our trigger
+$("a.trigger").on("click", function(){
+
+//slide/animate our nav onto the screen
+$("nav").slideToggle("fast");
+
+})
